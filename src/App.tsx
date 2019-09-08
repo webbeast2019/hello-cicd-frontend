cd1c7c9993b9f8751a3ced26882a18dfacec547c
yuvalbl <yuval.bar.levi@gmail.com>
2019-09-08 15:36:05 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const endpoint = 'http://localhost:4000/quote';
+
 const App: React.FC = () => {
+  const [quote, setQuote] = useState();
+  const [quoteSource, setQuoteSource] = useState();
+  
+  const getQuote = () => {
+    fetch(endpoint)
+      .then((res) => res.json())
+      .then((data: any) => {
+      setQuote(data.body);
+      setQuoteSource(data.source);
+    });
+  };
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Version 1.0.0 <img src={logo} className="App-logo" alt="logo"/>
       </header>
+      <main>
+        <div className="jumbotron">
+          <h1 className="display-4">Daily Quote:</h1>
+          <p className="lead">{quote}</p>
+          <p>{quoteSource}</p>
+          <button className="btn btn-primary btn-lg" onClick={getQuote}>Get a quote</button>
+        </div>
+      </main>
     </div>
   );
-}
+};
 
 export default App;
